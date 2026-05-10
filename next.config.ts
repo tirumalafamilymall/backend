fbbc7e26643ebc201e699e6817bc98c07fdb3ad6
Vishal <vishalk16801680@gmail.com>
2026-05-10 10:53:37 +0530

    Improved admin auth

--- next.config.ts
+++ next.config.ts
@@ -9,16 +9,12 @@ const nextConfig: NextConfig = {
       {
         // Apply these headers to all routes in your API
         source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { 
-  key: "Access-Control-Allow-Origin", 
-  value: "https://admin.tirumalafamilymall.com" 
-},
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
-        ]
+headers: [
+  { key: "Access-Control-Allow-Credentials", value: "true" },
+  { key: "Access-Control-Allow-Origin", value: "https://admin.tirumalafamilymall.com" }, 
+  { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+]
       }
     ]
   }
