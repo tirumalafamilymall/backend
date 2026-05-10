73f35fbabfe72e85e5617e8d7f02f3289f6b4b74
Vishal <vishalk16801680@gmail.com>
2026-05-10 09:47:55 +0530

    Added cors

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,24 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Using "*" allows any frontend to connect. For strict security, you can replace "*" with "https://tirumalafamilymall.com"
+          { key: "Access-Control-Allow-Origin", value: "*" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
+  }
+};
 
-export default nextConfig
+export default nextConfig;
